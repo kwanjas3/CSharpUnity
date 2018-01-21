6ae82e8209de576fb517151c21a5ed71f20d0008
kwanjas3 <kwanjas3@users.noreply.github.com>
2018-01-20 22:01:16 -0500

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rb;
+    public bool jump1;
+    public int forceConst = 10;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            jump1 = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+        if (jump1)
+        {
+            jump1 = false;
+            rb.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+            other.gameObject.SetActive(false);
+    }
+}
+//Destroy(other.gameObject);
